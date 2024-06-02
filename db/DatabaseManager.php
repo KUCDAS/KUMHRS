1d91963a285bce8a0a70c0e29da8967a93db1739
mdeniz20 <91716426+mdeniz20@users.noreply.github.com>
2024-06-02 22:55:15 +0300

    Statistics added

--- db/DatabaseManager.php
+++ db/DatabaseManager.php
@@ -148,6 +148,153 @@ class DatabaseManager {
         return $results;
     }
 
+    public function getTopThreeHospitals(){
+        $sql = "SELECT H.hospital_id, H.hname as hname, COUNT(*) AS appointment_count
+        FROM APPOINTMENT A
+        JOIN DOCTOR AS DO ON A.doctor_id = DO.doctor_id
+        JOIN DEPARTMENT AS DEP ON DO.department_id = DEP.department_id
+        JOIN HOSPITAL AS H ON DEP.hospital_id = H.hospital_id
+        GROUP BY H.hospital_id
+        ORDER BY appointment_count DESC
+        LIMIT 3";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $results = [];
+        if($result -> num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                array_push($results, $row);
+            }
+        }
+        return $results;
+    }
+
+    public function getTopDepartments(int $num){
+        $sql = "SELECT DEP.department_id, DEP.dname, COUNT(*) AS appointment_count
+        FROM APPOINTMENT A
+        JOIN DOCTOR AS DO ON A.doctor_id = DO.doctor_id
+        JOIN DEPARTMENT AS DEP ON DO.department_id = DEP.department_id
+        JOIN HOSPITAL AS H ON DEP.hospital_id = H.hospital_id
+        GROUP BY DEP.department_id
+        ORDER BY appointment_count DESC
+        LIMIT ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param('s', $num);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $results = [];
+        if($result -> num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                array_push($results, $row);
+            }
+        }
+        return $results;
+    }
+
+    public function getTopPatients(int $rid, int $num){
+        $sql = "SELECT PE.rid, PE.name, COUNT(*) AS count
+        FROM APPOINTMENT A
+        JOIN DOCTOR AS DO ON A.doctor_id = DO.doctor_id
+        JOIN DEPARTMENT DEP ON DO.department_id = DEP.department_id
+        JOIN HOSPITAL H ON DEP.hospital_id = H.hospital_id
+        JOIN PATIENT PA ON A.patient_id = PA.patient_id
+        JOIN PERSON PE ON PA.rid = PE.rid
+        WHERE H.hospital_id IN(SELECT H2.hospital_id FROM Person AS P2
+                       JOIN DOCTOR AS Doc ON P2.rid = Doc.rid
+                       JOIN Department AS De ON Doc.department_id = De.department_id
+                       JOIN Hospital AS H2 ON De.hospital_id = H2.hospital_id
+                       WHERE P2.rid = ?)
+        GROUP BY PE.rid 
+        ORDER BY count DESC
+        LIMIT ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param('ss', $rid,$num);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $results = [];
+        if($result -> num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                array_push($results, $row);
+            }
+        }
+        return $results;
+    }
+
+    public function getTopDoctors(int $rid, int $limit){
+        $sql = "SELECT DO.doctor_id, PE.name as name, COUNT(*) as appointment_count
+        FROM APPOINTMENT A
+        JOIN DOCTOR AS DO ON A.doctor_id = DO.doctor_id
+        JOIN DEPARTMENT AS DEP ON DO.department_id = DEP.department_id
+        JOIN HOSPITAL AS H ON DEP.hospital_id = H.hospital_id
+        JOIN PERSON AS PE ON DO.rid = PE.rid
+        WHERE H.hospital_id IN(SELECT H2.hospital_id FROM Person AS P2
+                       JOIN DOCTOR AS Doc ON P2.rid = Doc.rid
+                       JOIN Department AS De ON Doc.department_id = De.department_id
+                       JOIN Hospital AS H2 ON De.hospital_id = H2.hospital_id
+                       WHERE P2.rid = ?)
+        GROUP BY DO.doctor_id
+        LIMIT ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param('ss', $rid, $limit);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $results = [];
+        if($result -> num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                array_push($results, $row);
+            }
+        }
+        return $results;
+    }
+
+    public function getTopMedicines(int $limit){
+        $sql = "SELECT MD.mid, MD.medicine_name as medicine_name, COUNT(*) as prescription_count
+        FROM MedicineDetail MD
+        JOIN PrescriptionMedicine PM ON MD.mid = PM.mid
+        JOIN Prescription PR ON PM.prescription_id = PR.prescription_id
+        JOIN Appointment A ON PR.aid = A.aid
+        WHERE A.adate > DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH)
+        GROUP BY medicine_name
+        ORDER BY prescription_count DESC
+        LIMIT ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param('s', $limit);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $results = [];
+        if($result -> num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                array_push($results, $row);
+            }
+        }
+        return $results;
+    }
+
+    public function getMostVisitedHospital(int $rid, int $limit){
+        $sql = "SELECT H.hospital_id AS id, H.hname AS name, COUNT(*) AS count
+        FROM Hospital AS H
+        JOIN Department AS Dep ON Dep.hospital_id = H.hospital_id
+        JOIN Doctor AS Dr ON Dr.department_id = Dep.department_id
+        JOIN Appointment AS A ON A.doctor_id = Dr.doctor_id
+        JOIN Patient AS Pat ON Pat.patient_id = A.patient_id
+        JOIN Person AS Per ON Per.rid = Pat.rid
+        WHERE Per.rid = ?
+        GROUP BY H.hospital_id
+        ORDER BY count DESC
+        LIMIT ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param('ss', $rid, $limit);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $results = [];
+        if($result -> num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                array_push($results, $row);
+            }
+        }
+        return $results;
+    }
+
     public function getAllHospitals(){
         $sql = "SELECT H.hname AS hospital_name FROM Hospital as H";
         $stmt = $this->conn->prepare($sql);
